d8f796f2f3f450a274f0f6f18125a4277dc99b3d
Vinay <h421165@gmail.com>
2025-12-23 23:58:00 +0530

    Fix NLTK handling for Streamlit Cloud

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
+def load_nltk_resources():
+    try:
+        stopwords.words('english')
+    except LookupError:
+        nltk.download('stopwords')
+        nltk.download('punkt')
+    return stopwords.words('english')
+
+STOPWORDS = load_nltk_resources()
 ps = PorterStemmer()
 
 def transform_text(text):
@@ -20,7 +29,7 @@ def transform_text(text):
     y.clear()
     
     for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
+        if i not in STOPWORDS and i not in string.punctuation:
             y.append(i)
             
     text = y[:]
